c957071aedd6841ca11421b8c3b944f360fda9da
Tai Le <161448595+blackicecoffee@users.noreply.github.com>
2024-07-02 11:46:44 +0700

    add pdf handler

--- src/package/PDFHandler.py
+++ src/package/PDFHandler.py
@@ -1,8 +1,24 @@
 import package.CustomHandler as CustomHandler
+from ImageHandler import ImageHandler
+import pymupdf
 
 class PDFHandler(CustomHandler.CustomHandler):
     def __init__(self, type):
         super().__init__(type)
 
     def process(self, file):
-        pass
+        # Return result
+        result = []
+
+        # Image handler
+        imageHandler = ImageHandler()
+
+        with pymupdf.open(file) as doc:
+            for page_count, page in enumerate(doc):
+                pix = page.get_pixmap()
+                image_save = f"page_{page_count + 1}.png"
+                pix.save(image_save)
+                image_res = imageHandler.process(image_save)
+                result.append(image_res)
+
+        return result
