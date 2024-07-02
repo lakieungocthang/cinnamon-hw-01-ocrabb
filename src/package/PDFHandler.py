ac14e21eb50578969cec0a205db9cadc8ba8ba63
Tai Le <161448595+blackicecoffee@users.noreply.github.com>
2024-07-02 23:38:25 +0700

    add comment

--- src/package/PDFHandler.py
+++ src/package/PDFHandler.py
@@ -15,9 +15,14 @@ class PDFHandler(CustomHandler.CustomHandler):
 
         with pymupdf.open(file) as doc:
             for page_count, page in enumerate(doc):
+                # Convert a page to image
                 pix = page.get_pixmap()
+
+                # Save image
                 image_save = f"page_{page_count + 1}.png"
                 pix.save(image_save)
+
+                # Image processing
                 image_res = imageHandler.process(image_save)
                 result.append(image_res)
 
